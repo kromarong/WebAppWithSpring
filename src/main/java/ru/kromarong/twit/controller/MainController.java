dcd7ddcaacd56f851c1f8b05e0603c6a769ff4c6
kromarong <kromarong@gmail.com>
2019-06-17 14:30:24 +0300

    Add simple security support

--- src/main/java/ru/kromarong/twit/controller/MainController.java
+++ src/main/java/ru/kromarong/twit/controller/MainController.java
@@ -1,4 +1,4 @@
-package ru.kromarong.twit;
+package ru.kromarong.twit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,23 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ru.kromarong.twit.domain.Message;
 import ru.kromarong.twit.repos.MessageRepo;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
 
         Iterable<Message> messages = messageRepo.findAll();
@@ -32,7 +29,7 @@ public class GreetingController {
 
         return "main";
     }
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message = new Message(text, tag);
         messageRepo.save(message);
---
+++ src/main/java/ru/kromarong/twit/domain/Role.java
@@ -0,0 +1,5 @@
+package ru.kromarong.twit.domain;
+
+public enum Role {
+    USER;
+}
